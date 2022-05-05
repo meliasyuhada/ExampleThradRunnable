2044d91bb44444a6ffaa1bec162115c1beecefb4
Melia Syhada <meliasyuhada5@gmail.com>
2022-05-05 18:32:45 +0700

    Menggunakan Kelas Handler

--- app/src/main/java/id/com/diaryme/meliasepti/threadrunnable/MainActivity.kt
+++ app/src/main/java/id/com/diaryme/meliasepti/threadrunnable/MainActivity.kt
@@ -2,12 +2,25 @@ package id.com.diaryme.meliasepti.threadrunnable
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Message
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    // Menggunakan kelas handler
+    lateinit var mhandler: Handler
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // menggunakan heandler
+        mhandler = object  : Handler() {
+            override fun handleMessage(msg: Message) {
+                textview.text = msg?.data?.getString("counter")
+            }
+        }
 
         button.setOnClickListener {
         // ini ThreadRunnable
@@ -31,8 +44,14 @@ class MainActivity : AppCompatActivity() {
 //            println("i: $i")
 //        }
         for (i in 1..20) {
-            runOnUiThread(Runnable { textview.text = i.toString() })
-            println("i:$i")
+            // mengubah UI menggunakan Thread
+//            runOnUiThread(Runnable { textview.text = i.toString() })
+//            println("i:$i")
+
+    // menggunakan kelas Handler
+        var msg = Message.obtain()
+            msg.data.putString("counter", i.toString())
+            mhandler.sendMessage(msg)
             Thread.sleep(2000)
         }
     }
